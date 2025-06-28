0a4f4ed9fcdc1b2ecb2bc02c19a63778fe3fc93a
DhivyaSelvaraj <writemedivi@gmail.com>
2025-06-28 18:36:30 +0530

    Assignments

--- TW_Assignments/src/oops/Association/UserLaptop.java
+++ TW_Assignments/src/oops/Association/UserLaptop.java
@@ -1,15 +1,34 @@
 package oops.Association;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class UserLaptop {
     public static void main(String[] args) {
 
-        HardDisk h1 = new HardDisk("2 TB", "SSD");
-        Ram r1 = new Ram(64 , 3200);
-        Processor p1 = new Processor("M1 Air");
-        Laptop l1 = new Laptop(h1 , r1, p1);
-        l1.displayLaptopSpec();
+        List<Laptop> laptopList = new ArrayList<>();
+
+        laptopList.add(
+                        new Laptop( new HardDisk("2 TB", "SSD"),
+                        new Ram(64 , 3200),
+                        new Processor("M1 Air")
+                        ));
+
+        laptopList.add(new Laptop(
+                new HardDisk("1 TB", "Mechanical"),
+                new Ram(128, 2400),
+                new Processor("M3 Pro")
+        ));
+
+        int userCount = 0;
+        for(Laptop laptop : laptopList){
+            userCount ++;
+            System.out.println("**********  Laptop Specification for User " + userCount + " **************");
+            laptop.displayLaptopSpec();
+        }
+
 
     }
 }
